76a3d85d5face3ca6476fc9bec496909aacf5781
Vladislav <bakalavrat.789@gmail.com>
2023-01-30 19:03:43 +0200

    Update 02-Video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,12 +4,26 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(onPlay, 1000));
-
 function onPlay(data) {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
-    console.log(data)
+  localStorage.setItem('videoplayer-current-time', data.seconds);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.on('timeupdate', throttle(onPlay, 1000));
 
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+      
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
